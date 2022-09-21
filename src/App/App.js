58838bfaf9f4c337e4a0ef9208f3c88093f934a8
emmacintosh <emmammrussell@gmail.com>
2022-09-21 15:55:24 -0600

    Set up app file structure, create JS and CSS files for App/CharacterData/Form/Card/Note and set up basic functional skeleton and imports

---
+++ src/App/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react'
+import CharacterData from './CharacterData'
+import Form from './Form'
+import Card from './Card'
+import Note from './Note'
+
+class App extends Component() {
+  constructor() {
+    super()
+    this.state =  {
+    }
+  }
+  render = () => {
+    return (
+    <div>
+      
+    </div>) 
+  }
+}
+
+export default App;
---
+++ src/Note/Note.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Note.css';
+import Card from './Card'
+
+const Note = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Note
