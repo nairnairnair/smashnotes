cb7ed5e077dcb3efc2ced84335f1474eba964215
emmacintosh <emmammrussell@gmail.com>
2022-09-24 00:16:09 -0600

    Add dropdown menu, dropdown data and routing

---
+++ src/CharacterData/dropdownData.js
@@ -0,0 +1,108 @@
+const dropdownData = [
+    {
+        value: "1",
+        name: "Bowser",
+    },
+    {
+        value: "2",
+        name: "Captain Falcon",
+    },
+    {
+        value: "3",
+        name: "Donkey Kong",
+    },
+    {
+        value: "4",
+        name: "Dr. Mario",
+    },
+    {
+        value: "5",
+        name: "Falco",
+    },
+    {
+        value: "6",
+        name: "Fox",
+    },
+    {
+        value: "7",
+        name: "Ganondorf",
+    },
+    {
+        value: "8",
+        name: "Ice Climbers",
+    },
+    {
+        value: "9",
+        name: "Jigglypuff",
+    },
+    {
+        value: "10",
+        name: "Kirby",
+    },
+    {
+        value: "11",
+        name: "Link",
+    },
+    {
+        value: "12",
+        name: "Luigi",
+    },
+    {
+        value: "13",
+        name: "Mario",
+    },
+    {
+        value: "14",
+        name: "Marth",
+    },
+    {
+        value: "15",
+        name: "Mewtwo",
+    },
+    {
+        value: "16",
+        name: "Mr. Game & Watch",
+    },
+    {
+        value: "17",
+        name: "Ness",
+    },
+    {
+        value: "18",
+        name: "Peach",
+    },
+    {
+        value: "19",
+        name: "Pichu",
+    },
+    {
+        value: "20",
+        name: "Pikachu",
+    },
+    {
+        value: "21",
+        name: "Roy",
+    },
+    {
+        value: "22",
+        name: "Samus",
+    },
+    {
+        value: "23",
+        name: "Sheik",
+    },
+    {
+        value: "24",
+        name: "Yoshi",
+    },
+    {
+        value: "25",
+        name: "Young Link",
+    },
+    {
+        value: "26",
+        name: "Zelda",
+    },
+]
+
+export default dropdownData
